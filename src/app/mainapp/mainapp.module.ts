06b8146340b0a24692bad0c882db1a7279d146fa
CruelEngine <babhineethbhat@gmail.com>
2018-11-04 19:27:01 +0530

    few changes

--- src/app/mainapp/mainapp.module.ts
+++ src/app/mainapp/mainapp.module.ts
@@ -1,28 +1,42 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { MainappRoutingModule } from './mainapp-routing.module';
-import { NavigationComponent } from './navigation/navigation.component';
-import { MainappComponent } from './mainapp/mainapp.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MainappRoutingModule } from "./mainapp-routing.module";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { MainappComponent } from "./mainapp/mainapp.component";
+import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
-
-import { MatFormFieldModule , MatInputModule , MatCardModule} from '@angular/material';
-import { UserFriendsComponent } from './user-friends/user-friends.component';
-import { FriendsdatePipe } from './friendsdate.pipe'
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatListModule,
+  MatButtonModule
+} from "@angular/material";
+import { UserFriendsComponent } from "./user-friends/user-friends.component";
+import { FriendsdatePipe } from "./friendsdate.pipe";
 
 @NgModule({
   imports: [
     CommonModule,
-    MainappRoutingModule ,
-    MatFormFieldModule ,
-    MatInputModule ,
+    MainappRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatCardModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatListModule,
+    MatButtonModule
   ],
-  declarations: [NavigationComponent, MainappComponent, PagenotfoundComponent, UserProfileComponent, UserFriendsComponent, FriendsdatePipe]
+  declarations: [
+    NavigationComponent,
+    MainappComponent,
+    PagenotfoundComponent,
+    UserProfileComponent,
+    UserFriendsComponent,
+    FriendsdatePipe
+  ]
 })
-export class MainappModule { }
+export class MainappModule {}
--- src/app/mainapp/user-profile/user-profile.component.ts
+++ src/app/mainapp/user-profile/user-profile.component.ts
@@ -1,35 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/user.service';
-import { User } from '../../user';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/user.service";
+import { User } from "../../user";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.component.html",
+  styleUrls: ["./user-profile.component.scss"]
 })
 export class UserProfileComponent implements OnInit {
+  user: User;
+  userForm: FormGroup;
+  profileImage: string =
+    "https://material.angular.io/assets/img/examples/shiba2.jpg";
 
-  user : User;
-  userForm : FormGroup
-
-  constructor(private _userService : UserService , private _fb : FormBuilder) {
+  constructor(private _userService: UserService, private _fb: FormBuilder) {
     this.user = this._userService.savedUserPublisher().getValue();
 
-    if(this.user == null){
-      this.user = {name : '' , email : '' , mobile : '' , uid : '' , friendCount : '0' , image : ''};
-    }
+    // if(this.user == null){
+    //   thi
+    // }
   }
 
   ngOnInit() {
+    if (this.user == null) {
+      this.user = new User("", "", "", "", 0, "");
+    }
     this.userForm = this._fb.group({
-      'name' : [ {value : this.user.name ? this.user.name : '' , disabled : true},Validators.required],
-      'email' : [ {value : this.user.email , disabled : true} , Validators.required],
-      'mobile' : [ {value : this.user.mobile , disabled : true} , Validators.required],
-      'password' : [ { value : '*******' , disabled : true} , Validators.required]
+      name: [
+        { value: this.user.name ? this.user.name : "", disabled: true },
+        Validators.required
+      ],
+      email: [{ value: this.user.email, disabled: true }, Validators.required],
+      mobile: [
+        { value: this.user.mobile, disabled: true },
+        Validators.required
+      ],
+      password: [{ value: "*******", disabled: true }, Validators.required]
     });
   }
 
-  
 
+  editProfilePicture(event){
+    const selectedFiles : FileList = event.target.files;
+  }
 }
