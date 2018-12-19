f7b79c0fd7a20139791a7ffa0479eaa7e186b050
CruelEngine <babhineethbhat@gmail.com>
2018-12-19 22:40:26 +0530

    message form component and message component

--- src/app/chat/chat-message/chat-message.component.ts
+++ src/app/chat/chat-message/chat-message.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  Input
+} from "@angular/core";
+import { UserService } from "src/app/user.service";
 
 @Component({
   selector: "app-chat-message",
   templateUrl: "./chat-message.component.html",
-  styleUrls: ["./chat-message.component.scss"]
+  styleUrls: ["./chat-message.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChatMessageComponent implements OnInit {
-  constructor() {}
+  @Input() message: Message;
+  uid: string;
+  constructor(private _userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.uid = this._userService.savedUserPublisher().getValue().uid;
+  }
+
+  isReciever(message: Message) {
+    return this.uid === message.receiverUid;
+  }
+
+  isSender(message: Message) {
+    return this.uid === message.senderUid;
+  }
 }
